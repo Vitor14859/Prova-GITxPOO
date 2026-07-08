a564126c53f5841e1ce83624edff2622f005e689
Vitor14859 <guilherme24prates@gmail.com>
2026-07-08 15:53:24 -0300

    Update main.java

--- questao1pratica/main.java
+++ questao1pratica/main.java
@@ -1,13 +1,29 @@
 package questao1pratica;
 
 public class main {
-    public main(String[] args) {
+    public static void main(String[] args) {
 
-        Temporada t = new Temporada(2026, "temporada sim", 5, true);
+        Temporada t = new Temporada(2026, "temporada sim", 2, true);
         t.exibiriformacoes();
 
-        robo[] r = new robo[2];
+        progamador p2 = new progamador("Luis", 13, "sim");
+        progamador p1 = new progamador("Jair", 22, "não");
 
-        r1.
+        mecanico m1 = new mecanico("josé", 34, "ok", "ser bom" );
+    mecanico m2 = new mecanico("pedro", 45, "okay", "ser ruim");
+        
+        robo r1 = new robo("cristo", 30f, 20, true);
+
+
+        System.out.println("Equipe:");
+        System.out.println("progamadores: ");
+        p1.exibirInformacoes();
+        p2.exibirInformacoes();
+        System.out.println("mecanicos: ");
+        m1.exibirInformacoes();
+        m2.exibirInformacoes();
+        r1.exibirInformacoes();          
+
+
     }
 }
